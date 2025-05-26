c7cd26e97bfa5ed4298f99a54b57626d5e34d217
artusivictoria <vartusi@udesa.edu.ar>
2025-05-26 11:50:49 -0300

    StackNavigation

---
+++ proyectoFinalprog3/src/navigation/StackNavigation.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+import Login from "../screens/Login"
+import Profile from "../screens/Profile"
+import Register from "../screens/Register";
+const Stack = createNativeStackNavigator();
+
+function StackNavigation() {
+    return(
+        <Stack.Navigator>
+             <Stack.Screen 
+            name='Register' 
+            component={Register}
+            options={
+                {
+                    headerShown: false
+                }
+            }
+            />
+            <Stack.Screen 
+            name='Home' 
+            component={Home}
+            options={
+                {
+                    headerShown: false
+                }
+            }
+            />
+            <Stack.Screen
+                name='Login'
+                component={Login}
+                options={{
+                    headerShown:false
+                }}
+            />
+            <Stack.Screen
+                name='Profile'
+                component={Profile}
+                options={{
+                    headerShown:false
+                }}
+            />
+         
+        </Stack.Navigator>
+    )
+}
+
+export default StackNavigation

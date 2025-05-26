f3b90f95ee62e96cedaf5e6bf6489ef3a02548b2
artusivictoria <vartusi@udesa.edu.ar>
2025-05-26 12:09:40 -0300

    correcciones

--- proyectoFinalprog3/src/navigation/StackNavigation.js
+++ proyectoFinalprog3/src/navigation/StackNavigation.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/Home";
 import Login from "../screens/Login"
-import Profile from "../screens/Profile"
 import Register from "../screens/Register";
 const Stack = createNativeStackNavigator();
 
